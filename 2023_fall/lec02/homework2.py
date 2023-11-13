07762c80e517faefe62d86355f65379ca175c75f
st022345 <148079673+st022345@users.noreply.github.com>
2023-11-13 19:27:31 +0900

    Update homework2.py

--- 2023_fall/lec02/homework2.py
+++ 2023_fall/lec02/homework2.py
@@ -1,10 +1,10 @@
 a = "This is a python string"
 b = 7 % 3
-c = 7 % 3
+c = 55 * 234
 name1 = "KCGI"
 name2 = "Kyoto"
 d = name1 + ' @ ' + name2
-e =  round(3.458, 2)
+e = round(3.458, 2)
 f = str(57)
 g = 5==9
 
